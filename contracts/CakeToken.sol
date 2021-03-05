44fbc0cbbd58e3a3fa044551cb4ab0270fbf188e
kuandinya2021 <kuandinya2021@hotmail.com>
2021-03-06 00:35:17 +0700

    change cont5ract name

--- contracts/CakeToken.sol
+++ contracts/CakeToken.sol
@@ -5,7 +5,7 @@ pragma solidity 0.6.12;
 import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/BEP20.sol";
 
 // CakeToken with Governance.
-contract CakeToken is BEP20("BuddhaSwap Token", "Merit") {
+contract CakeToken is BEP20("MERIT Token", "MERIT") {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
